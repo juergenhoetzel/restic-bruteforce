c717f7072508dc92f9c7326e06441d6a8ab5ed7a
Juergen Hoetzel <juergen@hoetzel.info>
2020-12-18 17:34:22 +0100

    Initial import

---
+++ main.c
@@ -0,0 +1,161 @@
+#include "util.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#define PLEN 128
+
+typedef struct {
+  int N, r, p;
+  char *salt_bytes;
+  int salt_length;
+  char *data;
+  int data_length;
+  pthread_mutex_t get_work_lock;
+  int count;
+  clock_t clock;
+} work;
+
+int valid(EVP_PKEY_CTX *pctx, work *work, char *password) {
+  size_t keylen = 64;
+  char key[64];
+  assert(EVP_PKEY_CTX_set1_pbe_pass(pctx, password, strlen(password)) > 0);
+  assert(EVP_PKEY_CTX_set1_scrypt_salt(pctx, work->salt_bytes, work->salt_length) > 0);
+  assert(EVP_PKEY_CTX_set_scrypt_N(pctx, work->N) > 0);
+  assert(EVP_PKEY_CTX_set_scrypt_r(pctx, work->r) > 0);
+  assert(EVP_PKEY_CTX_set_scrypt_p(pctx, work->p) > 0);
+  assert(EVP_PKEY_derive(pctx, (unsigned char *)key, &keylen) > 0);
+  char *poly1305_key = key + 32;
+
+  char mac[16];
+
+  const char *ciphertext = work->data + 16;
+  const char *nonce = work->data;
+  int ciphertext_length = work->data_length - 32;
+  calculate_mac((unsigned char *)nonce, (unsigned char *)poly1305_key, (unsigned char *)ciphertext, ciphertext_length,
+                (unsigned char *)mac);
+  if (memcmp(mac, work->data + work->data_length - 16, 16) == 0) {
+    return 1;
+  }
+  return 0;
+}
+
+void *do_work(void *arg) {
+  work *w = (work *)arg;
+  EVP_PKEY_CTX *pctx = pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_SCRYPT, NULL);
+  assert(EVP_PKEY_derive_init(pctx) > 0);
+  char *password = malloc(PLEN);
+  /* loop stdin*/
+  size_t n = PLEN;
+  do {
+    pthread_mutex_lock(&w->get_work_lock);
+    n = getline(&password, &n, stdin);
+    if (w->clock) {
+      clock_t elapsed = (clock() - w->clock) / CLOCKS_PER_SEC;
+      if (elapsed > 20) {
+        printf("Checked %d passwords\n", w->count);
+        w->clock = clock();
+      }
+    }
+    w->count++;
+    pthread_mutex_unlock(&w->get_work_lock);
+    if (n == -1)
+      return 0; /* EOF */
+    password[n - 1] = 0;
+    if (valid(pctx, w, password)) {
+      printf("Found: %s\n", password);
+      /* FIXME: Conditional variables */
+      exit(0);
+    }
+  } while (1);
+  return 0;
+}
+
+void usage() {
+  fprintf(stderr, "Usage: restic-bruteforce [options] \n");
+  fprintf(stderr, "Bruteforce words on STDIN\n\n"
+                  "scryptoptions:\n\n"
+                  "  -n int\t(32768)\n"
+                  "  -r int\t(8)\n"
+                  "  -p int\t(7)\n\n"
+                  "  -v\t verbose output\n");
+}
+int main(int argc, char *argv[]) {
+  int n = 32768;
+  int r = 8;
+  int p = 7;
+
+  int c;
+  int verbose = 0;
+  while ((c = getopt(argc, argv, "hn:r:p:v")) != -1)
+    switch (c) {
+    case 'h':
+      usage();
+      exit(0);
+    case 'n':
+      n = atoi(optarg);
+      break;
+    case 'r':
+      r = atoi(optarg);
+      break;
+    case 'p':
+      p = atoi(optarg);
+      break;
+    case 'v':
+      verbose = 1;
+      break;
+    case '?':
+      usage();
+      exit(1);
+    }
+  if (argc - optind != 2) {
+    usage();
+    exit(1);
+  }
+
+  const char *salt = argv[optind];
+  const char *data_base64 = argv[optind + 1];
+  if (((strlen(salt) * 3) % 4) > 0) {
+    printf("Invalid Salt Padding: %s", salt);
+    exit(1);
+  }
+  if (((strlen(data_base64) * 3) % 4) > 0) {
+    printf("Invalid Data_Base64 Padding: %s", data_base64);
+    exit(1);
+  }
+  /* decode salt */
+  size_t salt_length = base64_decode_length(salt);
+  char *salt_bytes = malloc(salt_length);
+  base64_decode(salt, salt_bytes);
+
+  /* decode data_base64 */
+  size_t data_length = base64_decode_length(data_base64);
+  char *data = malloc(data_length);
+  base64_decode(data_base64, data);
+
+  work w = {.N = n,
+            .r = r,
+            .p = p,
+            .salt_length = salt_length,
+            .salt_bytes = salt_bytes,
+            .data = data,
+            .data_length = data_length,
+            .get_work_lock = PTHREAD_MUTEX_INITIALIZER,
+            .count = 0,
+            .clock = verbose ? clock() : 0};
+
+  int n_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+  printf("Using parameters (N=%d, r=%d, p=%d) on %d Threads\n", w.N, w.r, w.p, n_cpus);
+  pthread_t *workers = malloc(sizeof(pthread_t) * n_cpus);
+  int result;
+  for (int i = 0; i < n_cpus; i++)
+    pthread_create(workers + i, NULL, do_work, &w);
+  for (int i = 0; i < n_cpus; i++) {
+    pthread_join(workers[i], (void *)&result);
+  }
+  /* if we reach this: No password was found */
+  fprintf(stderr, "unlucky\n");
+  exit(1);
+}
---
+++ util.c
@@ -0,0 +1,84 @@
+#include <assert.h>
+#include <string.h>
+
+#include "util.h"
+#include <openssl/evp.h>
+/* FIXME: use meson to setup */
+#include "poly1305-donna/poly1305-donna.h"
+
+void hexprint(char *out, int len) {
+  for (int i = 0; i < len; i++) {
+    printf("%02X", (unsigned char)out[i]);
+  }
+  printf("\n");
+}
+
+char *hexstring(char *out, int len) {
+  char *s = malloc(len * 2 + 1);
+  for (int i = 0; i < len; i++)
+    sprintf(s + i * 2, "%02X", (unsigned char)out[i]);
+  return strdup(s);
+}
+
+size_t base64_decode_length(const char *b64input) {
+  size_t len = strlen(b64input), padding = 0;
+  if (b64input[len - 1] == '=' && b64input[len - 2] == '=')
+    padding = 2;
+  else if (b64input[len - 1] == '=')
+    padding = 1;
+  return (len * 3) / 4 - padding;
+}
+
+void base64_decode(const char *b64input, char *outbuf) {
+  BIO *bio_in = BIO_new_mem_buf(b64input, strlen(b64input));
+  BIO *b64_filter = BIO_new(BIO_f_base64());
+  BIO *bio = BIO_push(b64_filter, bio_in);
+  /* FIXME : FIXED BUFFER SIZE */
+  BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
+  BIO_read(bio, outbuf, base64_decode_length(b64input));
+  BIO_free_all(bio);
+}
+
+/*
+Freeing return string is up to the caller
+ */
+char *base64_encode(const char *in, int len) {
+  BIO *bmem = BIO_new(BIO_s_mem());
+  BIO *b64 = BIO_new(BIO_f_base64());
+  BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+  BIO_push(b64, bmem);
+  BIO_write(b64, in, len);
+  BIO_flush(b64);
+  BIO_set_close(bmem, BIO_NOCLOSE);
+  BUF_MEM *bufferPtr;
+  BIO_get_mem_ptr(bmem, &bufferPtr);
+  BIO_free_all(b64);
+  return strndup(bufferPtr->data, bufferPtr->length);
+}
+
+void poly1305_preparekey(unsigned char *nonce, unsigned char *key, unsigned char *prepared_key) {
+  unsigned char key_mask[16] = {0xff, 0xff, 0xff, 0x0f, 0xfc, 0xff, 0xff, 0x0f,
+                                0xfc, 0xff, 0xff, 0x0f, 0xfc, 0xff, 0xff, 0x0f};
+  /* mask key */
+  unsigned char masked_key[32];
+  memcpy(masked_key, key, 32);
+  for (int i = 0; i < 16; i++) {
+    masked_key[i + 16] &= key_mask[i];
+  }
+  /* FIXME Use k as aes key */
+  EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+  assert(ctx);
+  assert(EVP_EncryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, masked_key, NULL) == 1);
+  int out_len;
+  assert(1 == EVP_EncryptUpdate(ctx, prepared_key + 16, &out_len, nonce, 16));
+  assert(out_len == 16);
+  memcpy(prepared_key, masked_key + 16, 16);
+}
+
+/* key = k | r (32 bytes)*/
+void calculate_mac(unsigned char *nonce, const unsigned char *key, unsigned char *data, int data_len,
+                   unsigned char *mac) {
+  unsigned char prepared_key[32];
+  poly1305_preparekey(nonce, (unsigned char *)key, prepared_key);
+  poly1305_auth(mac, data, data_len, prepared_key);
+}
